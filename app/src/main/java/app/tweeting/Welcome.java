5dc0ab9d513ad162d4c6da7231cce1004036cfcd
Gaz1676 <gazfleming@gmail.com>
2017-09-24 22:29:09 +0100

    Added login/signup buttons pressed function

--- app/src/main/java/app/tweeting/Welcome.java
+++ app/src/main/java/app/tweeting/Welcome.java
@@ -1,8 +1,10 @@
 package app.tweeting;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -13,6 +15,18 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         mp = MediaPlayer.create(this, R.raw.welcome_chirp);
+        mp.start();
+    }
+
+    public void loginPressed(View view) {
+        startActivity (new Intent(this, Login.class));
+        mp = MediaPlayer.create(this, R.raw.valid_login);
+        mp.start();
+    }
+
+    public void signupPressed(View view) {
+        startActivity (new Intent(this, Signup.class));
+        mp = MediaPlayer.create(this, R.raw.valid_login);
         mp.start();
     }
 }
