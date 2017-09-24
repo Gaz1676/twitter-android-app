9cf993093ffa6446331b58a4e60d9211e42365fa
Gaz1676 <gazfleming@gmail.com>
2017-09-24 17:42:10 +0100

    Initial commit with Welcome page

---
+++ app/src/main/java/com/gary/mytweetapp/Tweet.java
@@ -0,0 +1,18 @@
+package com.gary.mytweetapp;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tweet extends AppCompatActivity {
+
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet);
+        mp = MediaPlayer.create(this, R.raw.welcome_chirp);
+        mp.start();
+    }
+}
