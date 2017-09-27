207afec411f5c2c167adb399214d332f4eab85fd
Gaz1676 <gazfleming@gmail.com>
2017-09-27 16:32:11 +0100

    Added function to when button is pressed

--- app/src/main/java/app/tweeting/Signup.java
+++ app/src/main/java/app/tweeting/Signup.java
@@ -5,6 +5,7 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
 
@@ -17,8 +18,18 @@ public class Signup extends AppCompatActivity {
     }
 
     public void signupPressed(View view) {
+        TextView firstName = (TextView)  findViewById(R.id.signupFirstName);
+        TextView lastName  = (TextView)  findViewById(R.id.signupLastName);
+        TextView email     = (TextView)  findViewById(R.id.signupEmail);
+        TextView password  = (TextView)  findViewById(R.id.signupPassword);
+
+        User user = new User (firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), password.getText().toString());
+
+        TweetApp app = (TweetApp) getApplication();
+        app.newUser(user);
         startActivity(new Intent(this, Login.class));
         mp = MediaPlayer.create(this, R.raw.valid_login);
         mp.start();
     }
+
 }
