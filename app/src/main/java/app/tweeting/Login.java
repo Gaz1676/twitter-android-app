ecf45ccf7e773a80e7c958b3f8168ca5238a6a4e
Gaz1676 <gazfleming@gmail.com>
2017-09-27 16:31:32 +0100

    Added function to when button is pressed

--- app/src/main/java/app/tweeting/Login.java
+++ app/src/main/java/app/tweeting/Login.java
@@ -2,9 +2,11 @@ package app.tweeting;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -17,8 +19,21 @@ public class Login extends AppCompatActivity {
     }
 
     public void loginPressed(View view) {
-        startActivity (new Intent(this, Login.class));
-        mp = MediaPlayer.create(this, R.raw.valid_login);
-        mp.start();
+        TweetApp app = (TweetApp) getApplication();
+
+        TextView email     = (TextView)  findViewById(R.id.loginEmail);
+        TextView password  = (TextView)  findViewById(R.id.loginPassword);
+
+        if (app.validUser(email.getText().toString(), password.getText().toString())) {
+            startActivity (new Intent(this, Tweet.class));
+            mp = MediaPlayer.create(this, R.raw.valid_login);
+            mp.start();
+        } else {
+            Toast toast = Toast.makeText(this, "Please sign up first!", Toast.LENGTH_SHORT);
+            toast.show();
+            mp = MediaPlayer.create(this, R.raw.invalid_login);
+            mp.start();
+        }
     }
+
 }
