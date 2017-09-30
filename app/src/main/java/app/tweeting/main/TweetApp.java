727674ea2b18bc01a471863136c9727d73cb8e50
Gaz1676 <gazfleming@gmail.com>
2017-09-30 19:05:57 +0100

    Moved file to main folder

--- app/src/main/java/app/tweeting/main/TweetApp.java
+++ app/src/main/java/app/tweeting/main/TweetApp.java
@@ -1,29 +1,32 @@
-package app.tweeting;
+package app.tweeting.main;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Application;
-import android.util.Log;
+import app.tweeting.activity.Tweet;
+import app.tweeting.model.Tweets;
+import app.tweeting.model.User;
 
 // any CRUD done through this class
 public class TweetApp extends Application {
-    public final int target = 10000;
-    public int totalDonated = 0;
-    //public List<Tweet> tweets = new ArrayList<>(); // creates new donations arraylist
+
+    public final int target = 140;
+    public int totalTweet = 0;
+    public List<Tweet> tweets = new ArrayList<>(); // creates new tweet arraylist
     public List<User> users = new ArrayList<>(); // creates new users arraylist
 
-   /* public boolean newTweets(Tweets tweets) {
-        boolean targetAchieved = totalDonated > target;
-        if (!targetAchieved) {
-            tweets.add(tweets);
-            totalDonated += tweets.amount;
-        } else {
+    public boolean newTweet(Tweets tweets) {
+        boolean targetAchieved = totalTweet > target;
+
             Toast toast = Toast.makeText(this, "Target Exceeded!", Toast.LENGTH_SHORT);
             toast.show();
-        }
         return targetAchieved;
-    }*/
+    }
+
 
     public void newUser(User user) {
         users.add(user);
