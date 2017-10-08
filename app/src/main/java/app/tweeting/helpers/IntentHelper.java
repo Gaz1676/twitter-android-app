9f1aa3987a6a153d0037a39daa432dbace8df0c0
Gaz1676 <gazfleming@gmail.com>
2017-10-08 15:54:01 +0100

    Tidied up IntentHelper to methods that I need

--- app/src/main/java/app/tweeting/helpers/IntentHelper.java
+++ app/src/main/java/app/tweeting/helpers/IntentHelper.java
@@ -5,26 +5,8 @@ import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v4.app.NavUtils;
 
-import java.io.Serializable;
-
 // useful location for methods that involved intents, either explicit or implicit.
 public class IntentHelper {
-    public static void startActivity(Activity parent, Class classname) {
-        Intent intent = new Intent(parent, classname);
-        parent.startActivity(intent);
-    }
-
-    public static void startActivityWithData(Activity parent, Class classname, String extraID, Serializable extraData) {
-        Intent intent = new Intent(parent, classname);
-        intent.putExtra(extraID, extraData);
-        parent.startActivity(intent);
-    }
-
-    public static void startActivityWithDataForResult(Activity parent, Class classname, String extraID, Serializable extraData, int idForResult) {
-        Intent intent = new Intent(parent, classname);
-        intent.putExtra(extraID, extraData);
-        parent.startActivityForResult(intent, idForResult);
-    }
 
     // to support enhanced navigation, we define an additional Helper method in the IntentHelper class
     public static void navigateUp(Activity parent) {
