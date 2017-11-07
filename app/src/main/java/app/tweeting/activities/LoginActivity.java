2a8eb591bddbbc6795e9299ba61a66a227219e59
Gaz1676 <gazfleming@gmail.com>
2017-11-07 21:58:03 +0000

    Added extra validation on signing up and login

--- app/src/main/java/app/tweeting/activities/LoginActivity.java
+++ app/src/main/java/app/tweeting/activities/LoginActivity.java
@@ -1,3 +1,9 @@
+/**
+ * Author: Gary Fleming
+ * Student No: 20019497
+ * Start Date: Sept 24th 2017
+ */
+
 package app.tweeting.activities;
 
 import android.content.Context;
@@ -9,15 +15,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import app.tweeting.R;
+import app.tweeting.helpers.ToastHelper;
+import app.tweeting.helpers.ValidateHelper;
 import app.tweeting.main.MyTweetApp;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     MediaPlayer mp;
+    boolean notValidated;
 
     /**
      * Login Activity Referenced from:
@@ -40,32 +48,47 @@ public class LoginActivity extends AppCompatActivity {
 
 
     // checks to see if the inputted data matches the user
+    // https://stackoverflow.com/questions/12266502/android-mediaplayer-stop-and-play
     public void login(View view) {
         MyTweetApp app = (MyTweetApp) getApplication();
         TextView email = (TextView) findViewById(R.id.loginEmail);
         TextView password = (TextView) findViewById(R.id.loginPassword);
 
 
-        if (app.validUser(email.getText().toString(), password.getText().toString())) {
-            startActivity(new Intent(this, TimelineActivity.class));
-            mp = MediaPlayer.create(this, R.raw.valid);
-            mp.start();
-        } else {
-            createToastMessage("Please sign up first!").show();
-
-            // https://stackoverflow.com/questions/13950338/how-to-make-an-android-device-vibrate
+        if (!ValidateHelper.isValidInput(email.getText().toString())) {
             Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
             v.vibrate(500);
-
             mp = MediaPlayer.create(this, R.raw.invalid);
             mp.start();
+            email.setError("Please enter a valid email");
+            notValidated = true;
+
+
+        } else if (!ValidateHelper.isValidEmail(email.getText().toString())) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+            mp = MediaPlayer.create(this, R.raw.invalid);
+            mp.start();
+            email.setError("Please enter a valid email");
+            notValidated = true;
+
+
+        } else if (!ValidateHelper.isValidInput(password.getText().toString())) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+            mp = MediaPlayer.create(this, R.raw.invalid);
+            mp.start();
+            password.setError("Please enter password");
+            notValidated = true;
+
+
+        } else if (app.validUser(email.getText().toString(), password.getText().toString())) {
+            mp = MediaPlayer.create(this, R.raw.valid);
+            mp.start();
+
+            ToastHelper.createToastMessage(this, "Welcome to MyTweetApp!");
+
+            startActivity(new Intent(this, TimelineActivity.class));
         }
-    }
-
-
-
-    // created a helper method for Toast response
-    private Toast createToastMessage(String string) {
-        return Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT);
     }
 }
--- app/src/main/java/app/tweeting/activities/SignupActivity.java
+++ app/src/main/java/app/tweeting/activities/SignupActivity.java
@@ -1,3 +1,9 @@
+/**
+ * Author: Gary Fleming
+ * Student No: 20019497
+ * Start Date: Sept 24th 2017
+ */
+
 package app.tweeting.activities;
 
 import android.content.Context;
@@ -9,9 +15,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import app.tweeting.R;
+import app.tweeting.helpers.ToastHelper;
+import app.tweeting.helpers.ValidateHelper;
 import app.tweeting.main.MyTweetApp;
 import app.tweeting.models.User;
 
@@ -40,6 +47,8 @@ public class SignupActivity extends AppCompatActivity {
 
 
     // details of new user created on sign up and saved in MyTweetApp Object
+    // https://stackoverflow.com/questions/13950338/how-to-make-an-android-device-vibrate
+    // https://stackoverflow.com/questions/12266502/android-mediaplayer-stop-and-play
     public void signup(View view) {
         TextView firstName = (TextView) findViewById(R.id.signupFirstName);
         TextView lastName = (TextView) findViewById(R.id.signupLastName);
@@ -47,49 +56,73 @@ public class SignupActivity extends AppCompatActivity {
         TextView password = (TextView) findViewById(R.id.signupPassword);
 
 
-        // validation check to see if email matches regex pattern
-        if (isValidEmail(email.getText().toString().trim())) {
-            User user = new User(firstName.getText().toString(), lastName.getText().toString(),
-                    email.getText().toString(), password.getText().toString());
+        if (!ValidateHelper.isValidInput(firstName.getText().toString())) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+            mp = MediaPlayer.create(this, R.raw.invalid);
+            mp.start();
+            firstName.setError("Please input first name");
 
+
+        } else if (!ValidateHelper.isValidName(firstName.getText().toString())) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+            mp = MediaPlayer.create(this, R.raw.invalid);
+            mp.start();
+            firstName.setError("Only use letters");
+
+
+        } else if (!ValidateHelper.isValidInput(lastName.getText().toString())) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+            mp = MediaPlayer.create(this, R.raw.invalid);
+            mp.start();
+            lastName.setError("Please input last name");
+
+
+        } else if (!ValidateHelper.isValidName(lastName.getText().toString())) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+            mp = MediaPlayer.create(this, R.raw.invalid);
+            mp.start();
+            lastName.setError("Only use letters");
+
+
+        } else if (!ValidateHelper.isValidEmail(email.getText().toString())) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+            mp = MediaPlayer.create(this, R.raw.invalid);
+            mp.start();
+            email.setError("Please enter a valid email");
+
+
+        } else if (!ValidateHelper.isValidInput(email.getText().toString())) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+            mp = MediaPlayer.create(this, R.raw.invalid);
+            mp.start();
+            email.setError("Please enter a valid email");
+
+
+        } else if (!ValidateHelper.isValidInput(password.getText().toString())) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+            mp = MediaPlayer.create(this, R.raw.invalid);
+            mp.start();
+            password.setError("Please enter password");
+
+
+        } else {
+            User user = new User(firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), password.getText().toString());
 
             MyTweetApp app = (MyTweetApp) getApplication();
             app.newUser(user);
             startActivity(new Intent(this, TimelineActivity.class));
 
-            createToastMessage("Welcome to MyTweetApp!").show();
-
             mp = MediaPlayer.create(this, R.raw.valid);
             mp.start();
 
-        } else {
-            // https://stackoverflow.com/questions/13950338/how-to-make-an-android-device-vibrate
-            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            v.vibrate(500);
-
-            createToastMessage("Please enter valid email").show();
-
-            mp = MediaPlayer.create(this, R.raw.invalid);
-            mp.start();
+            ToastHelper.createToastMessage(this, "Welcome to MyTweetApp!");
         }
-    }
-
-
-
-    // helper method for Toast response
-    private Toast createToastMessage(String string) {
-        return Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT);
-    }
-
-    // helper method to check email validation pattern
-    // https://stackoverflow.com/questions/624581/what-is-the-best-java-email-address-validation-method
-    public boolean isValidEmail(String email) {
-        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]" +
-                "+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\." +
-                "[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z" +
-                "\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-        java.util.regex.Matcher m = p.matcher(email);
-        return m.matches();
     }
 }
